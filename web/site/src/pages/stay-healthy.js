ba1b49d2a33ce7d782702dc2c9691cbb9995a299
Nathan LeRoy <NLeRoy917@gmail.com>
2020-09-07 21:02:51 -0400

    add google analytixs

--- web/site/src/pages/stay-healthy.js
+++ web/site/src/pages/stay-healthy.js
@@ -246,7 +246,6 @@ const StayHealthyPage = () => {
               </>
             }
             </SectionContent>
-            <ImgLarge src={social_bubbles} />
           </SectionWrapper>
           <br></br>
         <SectionWrapper mobile={mobile}>
