d3aac37a0dff6b59293d07fdcb6a89efcb1c994f
Nathan LeRoy <NLeRoy917@gmail.com>
2020-09-08 20:40:27 -0400

    try to fix og:image issues

--- web/site/gatsby-ssr.js
+++ web/site/gatsby-ssr.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+const _ = require("lodash");
+
+    // cleanup html before render
+    exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+      const headComponents = getHeadComponents();
+      replaceHeadComponents(omitDeep(headComponents, ["data-react-helmet"]));
+    };
+
+    /**
+     * remove properties from collection deep
+     * @param collection
+     * @param excludeKeys
+     * @returns {any}
+     */
+    const omitDeep = (collection, excludeKeys) =>
+      _.cloneDeepWith(collection, value => {
+        if (value && typeof value === "object") {
+          for (const key of excludeKeys) {
+            try {
+              delete value[key];
+            } catch (_) {
+              // console.log("ignore", _);
+            }
+          }
+        }
+      });
