12a637de6edf28404159e1c6509ce2b4b9b0ae94
Nathan LeRoy <NLeRoy917@gmail.com>
2020-09-09 09:56:50 -0400

    push opengraph meta tags to top of <head>

--- web/site/gatsby-ssr.js
+++ web/site/gatsby-ssr.js
@@ -5,29 +5,35 @@
  */
 
 // You can delete this file if you're not using it
-const _ = require("lodash");
+const React = require("react")
+const { Helmet } = require("react-helmet")
 
-    // cleanup html before render
-    exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-      const headComponents = getHeadComponents();
-      replaceHeadComponents(omitDeep(headComponents, ["data-react-helmet"]));
-    };
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic()
+  setHtmlAttributes(helmet.htmlAttributes.toComponent())
+  setBodyAttributes(helmet.bodyAttributes.toComponent())
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ])
+}
 
-    /**
-     * remove properties from collection deep
-     * @param collection
-     * @param excludeKeys
-     * @returns {any}
-     */
-    const omitDeep = (collection, excludeKeys) =>
-      _.cloneDeepWith(collection, value => {
-        if (value && typeof value === "object") {
-          for (const key of excludeKeys) {
-            try {
-              delete value[key];
-            } catch (_) {
-              // console.log("ignore", _);
-            }
-          }
-        }
-      });
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  headComponents.sort((x, y) => {
+    if (x.props && x.props["data-react-helmet"]) {
+      return -1
+    } else if (y.props && y.props["data-react-helmet"]) {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
