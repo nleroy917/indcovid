4519e723b432d985c1b643e771c521a7370045d3
Nathan LeRoy <NLeRoy917@gmail.com>
2021-01-14 20:57:39 -0500

    fix styling issues

--- web/site/src/pages/vaccine.js
+++ web/site/src/pages/vaccine.js
@@ -215,14 +215,14 @@ const VaccinePage = () => {
                   Vaccine Safety
               </SectionTitle>
               <SectionSubTitle>
-                  How was the vaccine evaluated for safety?
+                  <b>How was the vaccine evaluated for safety?</b>
               </SectionSubTitle>
               <SectionContent>
               New medications, vaccines (shots), and treatments may make you feel uncertain, but they are studied and evaluated with safety and effectiveness as the top priority. Vaccines (shots) undergo rigorous testing and must meet safety standards before being released. In addition to the clinical trials completed as part of the routine process, the vaccines (shots) will continue to be evaluated by the manufacturers and researchers over time in the real world. More information is available at <InlineLink href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/safety.html.">https://www.cdc.gov/coronavirus/2019-ncov/vaccines/safety.html</InlineLink>.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  Does this vaccine cause any serious side effects like infertility or autism?
+                  <b>Does this vaccine cause any serious side effects like infertility or autism?</b>
               </SectionSubTitle>
               <SectionContent>
               It has been scientifically proven that vaccines and vaccine ingredients do not cause or exacerbate autism or other developmental disabilities. There is no scientific or clinical evidence demonstrating that the COVID-19 vaccine will cause serious side effects. Some people may experience some discomfort in the days following their immunization. These discomforts can be an indication the body’s immune system is working with the vaccine to develop antibodies. These discomforts should only last a few days and may include:
@@ -235,49 +235,49 @@ const VaccinePage = () => {
                 </List>
               </SectionContent>
               <SectionSubTitle>
-                  The vaccine was developed very quickly, how do we know what the long term effects are? 
+                  <b>The vaccine was developed very quickly, how do we know what the long term effects are? </b>
               </SectionSubTitle>
               <SectionContent>
               Vaccines (shots) have been around for a long time, and the science used to create the COVID-19 vaccines (shots) has been studied for years. While the COVID-19 vaccine (shot) is new, the science used to create it is not. The COVID-19 vaccines (shots) have been thoroughly tested to make sure they are safe and effective. The <InlineLink href="https://www.fda.gov/emergency-preparedness-and-response/coronavirus-disease-2019-covid-19/covid-19-vaccines">FDA</InlineLink> authorized the vaccine (shot) under an Emergency Use Authorization (EUA), meaning the vaccine (shot) was proven safe and effective. The vaccine (shot) would not be approved for use if it were harmful or unsafe.  The COVID-19 vaccines (shots) took much less time than normal to be released because they were made at the same time as they were being studied and approved, rather than afterward, to save time. They still had the same strict safety measures and reviews as usual. No steps were skipped or shortened.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  Has the vaccine been tested among all racial/ethnic groups?
+                  <b>Has the vaccine been tested among all racial/ethnic groups?</b>
               </SectionSubTitle>
               <SectionContent>
               Both Pfizer and Moderna scientists worked hard to make sure there was a diverse group of people in the vaccine trials. Learn more about Pfizer <InlineLink href="https://www.pfizer.com/news/press-release/press-release-detail/pfizer-and-biontech-conclude-phase-3-study-covid-19-vaccine">here</InlineLink> and Moderna <InlineLink href="https://investors.modernatx.com/news-releases/news-release-details/moderna-announces-primary-efficacy-analysis-phase-3-cove-study)">here</InlineLink>.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  Will the COVID vaccine have any effect on the medications I am taking?
+                  <b>Will the COVID vaccine have any effect on the medications I am taking?</b>
               </SectionSubTitle>
               <SectionContent>
               There are no known interactions between any medications and the COVID-19 vaccine (shot). If you have specific concerns, talk to your healthcare provider.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  How long after I get the vaccine is it effective?
+                  <b>How long after I get the vaccine is it effective?</b>
               </SectionSubTitle>
               <SectionContent>
               CDC guidance says it typically takes a few weeks for the body to build immunity after vaccination.We are still learning about the length of time both natural immunity (immunity the body develops after an illness) and vaccine-induced immunity for COVID-19 will last. It is important to continue to wear your mask over your nose and mouth, practice social distancing, and continue to wash your hands and use hand sanitizer often. You should also wear a mask because although the vaccine (shot) will protect you from getting sick, it will not keep you from spreading COVID-19 to others.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  Will the vaccine work against the new strain of coronavirus?
+                  <b>Will the vaccine work against the new strain of coronavirus?</b>
               </SectionSubTitle>
               <SectionContent>
               Scientists are studying this now. There is no evidence that there are changes in vaccine (shot) effectiveness with the new variants, and most experts believe this is unlikely to happen because of science behind how the body attacks the virus. For more information on COVID-19 variants, please visit <InlineLink href="https://www.cdc.gov/coronavirus/2019-ncov/transmission/variant.html">https://www.cdc.gov/coronavirus/2019-ncov/transmission/variant.html</InlineLink>.  
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  Does the vaccine contain a microchip?
+                  <b>Does the vaccine contain a microchip?</b>
               </SectionSubTitle>
               <SectionContent>
               The vaccine (shot) does not contain a microchip or any other sort of device. It is impossible for a microchip to be placed in a vaccine (shot). It is also impossible that a microchip placed in the body can be tracked or used to control the body in any way.
               </SectionContent>
               <br></br>
               <SectionSubTitle>
-                  I don't have insurance. How will I pay for the vaccine?
+                  <b>I don't have insurance. How will I pay for the vaccine?</b>
               </SectionSubTitle>
               <SectionContent>
               The vaccine (shot) is free. Vaccination (shot) providers can charge an administration fee. Vaccination (shot) providers can get this fee paid back by the patient’s public or private insurance company or, for uninsured patients, by the <InlineLink href="https://www.hhs.gov/coronavirus/cares-act-provider-relief-fund/index.html">Health Resources and Services Administration’s Provider Relief Fund</InlineLink>.
